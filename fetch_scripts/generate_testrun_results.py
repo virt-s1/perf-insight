b5c98f21bdad3bad9da3011ceaf1fbcb855c448d
Charles Shih <schrht@gmail.com>
2020-12-07 18:33:54 +0800

    implement the json query with jq.

--- fetch_scripts/generate_testrun_results.py
+++ fetch_scripts/generate_testrun_results.py
@@ -9,6 +9,7 @@ import json
 import yaml
 import os
 import pandas as pd
+from jq import jq
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
@@ -62,9 +63,6 @@ class testrun_report_generator():
         with open(ARGS.datastore, 'r') as f:
             self.datastore = json.load(f)
 
-        print(self.config)
-        exit(0)
-        
         # parse parameters
         self.output = ARGS.output
         self.output_format = ARGS.output_format
@@ -74,46 +72,108 @@ class testrun_report_generator():
             fname = 'testrun_report.csv'
             self.output = os.path.join(fpath, fname)
 
+        # init
+        self.datatable = []
         self.dataframe = None
+        self._parse_data()
+        exit(0)
         self._build_dataframe()
 
-    def _build_dataframe(self):
-        table = []
+    def _parse_data(self):
+        """Parse data from the datastore.
+
+        Input:
+        - self.datastore: datastore.
+        - self.config: customized configuration.
+        Output:
+        - self.datatable: datatable to be generated.
+        """
+        def _get_value_metadata(cfg, data=None):
+            """Get value from metadata."""
+            pass
+
+        def _get_value_datastore(cfg, data=None):
+            """Get value(s) from datastore."""
+            # jq().transform() returns a list of string(s)
+            res = jq(cfg['jqexpr']).transform(data, multiple_output=True)
+
+            # multiply the factor if available
+            if 'factor' in cfg:
+                res = [x * cfg['factor'] for x in res]
+
+            # return the value or the whole list
+            if len(res) == 1:
+                return res[0]
+            else:
+                return res
+
+        def _get_value_auto(cfg, data=None):
+            """Caluclate value."""
+            pass
+
+        def _get_value_unknown(cfg, data=None):
+            """Unknown error."""
+            print('ERROR: Unknown type in "source", config = "%s".' % cfg)
+            exit(1)
+
+        self.config
+        self.datastore
+        self.datatable = []
+
+        splits = []
+        data = {}
+
+        switch = {
+            'metadata': _get_value_metadata,
+            'datastore': _get_value_datastore,
+            'auto': _get_value_auto,
+        }
+
+        # generate rows of the datatable
         for iterdata in self.datastore:
-            data = {}
+            # generate one row
+            for cfg in self.config['columns']:
+                name = cfg['name']
+                value = switch.get(cfg['source'], _get_value_unknown)(cfg,
+                                                                      iterdata)
+                print(name, ' = ', value)
 
-            # get keys
-            data['RW'] = iterdata['iteration_data']['parameters']['benchmark'][
-                0]['rw']
-            data['BS'] = iterdata['iteration_data']['parameters']['benchmark'][
-                0]['bs']
-            data['IODepth'] = iterdata['iteration_data']['parameters'][
-                'benchmark'][0]['iodepth']
-            data['Numjobs'] = iterdata['iteration_data']['parameters'][
-                'benchmark'][0]['numjobs']
+                continue
 
-            # get kpis
-            for client in iterdata['iteration_data']['throughput']['iops_sec']:
-                if client['client_hostname'] == 'all':
-                    iops = [x['value'] for x in client['samples']]
-            for client in iterdata['iteration_data']['latency']['lat']:
-                if client['client_hostname'] == 'all':
-                    lat = [x['value'] / 1000000 for x in client['samples']]
-            for client in iterdata['iteration_data']['latency']['clat']:
-                if client['client_hostname'] == 'all':
-                    clat = [x['value'] / 1000000 for x in client['samples']]
+            # # get keys
+            # data['RW'] = iterdata['iteration_data']['parameters']['benchmark'][
+            #     0]['rw']
+            # data['BS'] = iterdata['iteration_data']['parameters']['benchmark'][
+            #     0]['bs']
+            # data['IODepth'] = iterdata['iteration_data']['parameters'][
+            #     'benchmark'][0]['iodepth']
+            # data['Numjobs'] = iterdata['iteration_data']['parameters'][
+            #     'benchmark'][0]['numjobs']
 
-            # split into per sample
-            data['Sample'] = 0
-            for data['IOPS'], data['LAT(ms)'], data['CLAT(ms)'] in zip(
-                    iops, lat, clat):
-                data['Sample'] += 1
-                data['Path'] = os.path.join(iterdata['path_lv_1'],
-                                            iterdata['path_lv_2'],
-                                            'sample%d' % data['Sample'])
+            # # get kpis
+            # for client in iterdata['iteration_data']['throughput']['iops_sec']:
+            #     if client['client_hostname'] == 'all':
+            #         iops = [x['value'] for x in client['samples']]
+            # for client in iterdata['iteration_data']['latency']['lat']:
+            #     if client['client_hostname'] == 'all':
+            #         lat = [x['value'] / 1000000 for x in client['samples']]
+            # for client in iterdata['iteration_data']['latency']['clat']:
+            #     if client['client_hostname'] == 'all':
+            #         clat = [x['value'] / 1000000 for x in client['samples']]
 
-                # save this item
-                table.append(data.copy())
+            # # split into per sample
+            # data['Sample'] = 0
+            # for data['IOPS'], data['LAT(ms)'], data['CLAT(ms)'] in zip(
+            #         iops, lat, clat):
+            #     data['Sample'] += 1
+            #     data['Path'] = os.path.join(iterdata['path_lv_1'],
+            #                                 iterdata['path_lv_2'],
+            #                                 'sample%d' % data['Sample'])
+
+            #     # save this item
+            #     table.append(data.copy())
+
+    def _build_dataframe(self):
 
         self.dataframe = pd.DataFrame(table)
 
