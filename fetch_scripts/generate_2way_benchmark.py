7083e6f7c9d73dd5b57fb7a0566143dd015b7798
Charles Shih <schrht@gmail.com>
2020-12-14 14:30:25 +0800

    Add logic to init the report dataframe.

--- fetch_scripts/generate_2way_benchmark.py
+++ fetch_scripts/generate_2way_benchmark.py
@@ -64,7 +64,7 @@ class benchmark_comparison_generator():
         for item in self.keys_cfg:
             if not item.get('name'):
                 print('Error: "name" must be given.')
-                return (1)
+                return 1
             if 'from' not in item:
                 item['from'] = item['name']
             if 'unit' not in item:
@@ -74,7 +74,7 @@ class benchmark_comparison_generator():
         for item in self.kpis_cfg:
             if not item.get('name'):
                 print('Error: "name" must be given.')
-                return (1)
+                return 1
             if 'from' not in item:
                 item['from'] = item['name']
             if 'unit' not in item:
@@ -112,29 +112,70 @@ class benchmark_comparison_generator():
             self.output = os.path.join(fpath, fname)
 
         # init
-        self.datatable = []
-        self.dataframe = None
+        self.df_report = None
         self._parse_data()
-        self._build_dataframe()
+
+    def _init_df_report(self):
+        """Init the report dataframe.
+
+        Create a basic report dataframe with:
+        1. KEYs and KPIs as columns;
+        2. Deduplicated KEYs from test dataframe as rows.
+
+        Input:
+            - self.df_test: dataframe for the TEST samples.
+            - self.df_base: dataframe for the BASE samples.
+            - self.keys_cfg: customized configuration for KEYs.
+            - self.kpis_cfg: customized configuration for KPIs.
+
+        Updates:
+            - self.df_report: report dataframe to be updated.
+        """
+        # create a report dataframe with KEY columns only
+        keys_name = [x['name'] for x in self.keys_cfg]
+        keys_from = [x['from'] for x in self.keys_cfg]
+
+        # tailer the report dataframe from the test dataframe
+        self.df_report = self.df_test[keys_from].drop_duplicates()
+
+        # rename the columns
+        for key_name, key_from in zip(keys_name, keys_from):
+            if key_name != key_from:
+                self.df_report.rename(columns={key_from: key_name},
+                                      inplace=True)
+
+        # sort the report dataframe and reset its index
+        self.df_report = self.df_report.sort_values(by=keys_name)
+        self.df_report = self.df_report.reset_index().drop(columns=['index'])
+
+        # expaned the report dataframe with KPI columns
+        for kpi_cfg in self.kpis_cfg:
+            expansion = [
+                'T-mean', 'T-%sd', 'B-mean', 'B-%sd', '%diff', 'sign',
+                'speculate'
+            ]
+            for suffix in expansion:
+                self.df_report.insert(len(self.df_report.columns),
+                                      kpi_cfg['name'] + '-' + suffix, 0)
 
     def _parse_data(self):
         """Parse data from the testrun results.
 
         Input:
-        - self.df_test: results for TEST testrun.
-        - self.df_base: results for BASE testrun.
-        - self.config: customized configuration.
-        Output:
-        - self.datatable: datatable to be generated.
+            - self.df_test: dataframe for the TEST samples.
+            - self.df_base: dataframe for the BASE samples.
+            - self.config: customized configuration.
+        Updates:
+            - self.df_report: report dataframe to be updated.
         """
-        pass
-
-    def _build_dataframe(self):
-        self.dataframe = pd.DataFrame(self.datatable)
+        # init the report dataframe
+        self._init_df_report()
+        #self._fill_df_report()
 
     def dump_to_csv(self):
         with open(self.output, 'w') as f:
-            f.write(self.dataframe.to_csv())
+            pass
+            #f.write(self.dataframe.to_csv())
 
     def show_vars(self):
         """Print the value of varibles to the stdout."""
@@ -145,12 +186,12 @@ class benchmark_comparison_generator():
         # _show('self.config', self.config)
         # _show('self.keys_cfg', self.keys_cfg)
         # _show('self.kpis_cfg', self.kpis_cfg)
-        # _show('self.df_test', self.df_test)
+        _show('self.df_test', self.df_test)
         # _show('self.df_base', self.df_base)
         # _show('self.output', self.output)
         # _show('self.output_format', self.output_format)
-        # _show('self.datatable', self.datatable)
-        # _show('self.dataframe', self.dataframe)
+        # _show('self.df_params', self.df_params)
+        _show('self.df_report', self.df_report)
 
 
 if __name__ == '__main__':
