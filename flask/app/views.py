adc948077038ecfb5175a0771e43d03ff5a69bc5
Charles Shih <schrht@gmail.com>
2021-01-06 16:25:33 +0800

    rename testrun_metadata.json

--- flask/app/views.py
+++ flask/app/views.py
@@ -42,9 +42,9 @@ def jupiter_prepare(baserun, testrun, target_dir):
     baserun_dir = DATA_PATH + '/testruns/' + baserun
     testrun_dir = DATA_PATH + '/testruns/' + testrun
     shutil.copy(baserun_dir + '/datastore.json', target_dir + '/base.datastore.json')
-    shutil.copy(baserun_dir + '/metadata.json', target_dir + '/base.testrun_metadata.json')
+    shutil.copy(baserun_dir + '/metadata.json', target_dir + '/base.metadata.json')
     shutil.copy(testrun_dir + '/datastore.json', target_dir + '/test.datastore.json')
-    shutil.copy(testrun_dir + '/metadata.json', target_dir + '/test.testrun_metadata.json')
+    shutil.copy(testrun_dir + '/metadata.json', target_dir + '/test.metadata.json')
 
 def generate_dirname():
     num_list = []
