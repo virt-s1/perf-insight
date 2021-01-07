44860d779b85762be2c5d93d62eafcfb349933da
Xiao Liang <xiliang@redhat.com>
2021-01-07 09:39:21 +0800

    flask: add progress bar while waiting generate jupiter report
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -6,6 +6,8 @@ from sqlalchemy import event
 from sqlalchemy.engine import Engine
 
 from app.commen import MyIndexView
+import time
+from flask import Response
 
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
@@ -23,5 +25,16 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
+
+@app.route('/progress')
+def progress():
+	def generate():
+		x = 100
+		while x >= 0:
+			yield "data:" + str(x) + "\n\n"
+			x = x - 1
+			time.sleep(1)
+
+	return Response(generate(), mimetype= 'text/event-stream')
 
 from . import models, views  # noqa
