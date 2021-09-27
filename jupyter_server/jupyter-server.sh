0d19edf5da1c4bcd09b00c24817e1e4c0ac0d3ff
Charles Shih <schrht@gmail.com>
2021-09-27 16:41:16 +0800

    Start Flask in jupyter server container.

--- jupyter_server/jupyter-server.sh
+++ jupyter_server/jupyter-server.sh
@@ -14,7 +14,7 @@
 #       AUTHOR: Charles Shih (schrht@gmail.com)
 # ORGANIZATION: ---
 #      CREATED: Fri Sep 24 11:37:17 AM CST 2021
-#     REVISION: ---
+#     REVISION: Mon Sep 27 04:25:40 PM CST 2021
 #==============================================================================
 
 trap cleanup SIGINT SIGTERM ERR EXIT
@@ -24,7 +24,11 @@ cleanup() {
     exit 0
 }
 
+# Start Flask as API server
+cd /opt/perf-insight/jupyter_server/
+flask run --host 0.0.0.0 --port 8880
+
 # Start JupyterLab
-mkdir -p /app/workspace
-jupyter-lab -y --allow-root --no-browser --ip 0.0.0.0 --port 8888 \
-    --notebook-dir=/app/workspace --collaborative
+# mkdir -p /app/workspace
+# jupyter-lab -y --allow-root --no-browser --ip 0.0.0.0 --port 8888 \
+#     --notebook-dir=/app/workspace --collaborative
