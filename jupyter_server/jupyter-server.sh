85163078b623ee82d70e1186b8ee75dd29ff0e9c
Charles Shih <schrht@gmail.com>
2021-10-18 09:13:19 +0800

    Update the container setting for Jupyter Server.

--- jupyter_server/jupyter-server.sh
+++ jupyter_server/jupyter-server.sh
@@ -5,7 +5,7 @@
 #
 #        USAGE: ./jupyter-server.sh
 #
-#  DESCRIPTION: Foreground the JupyterLab service.
+#  DESCRIPTION: Foreground the API service.
 #
 #      OPTIONS: ---
 # REQUIREMENTS: ---
@@ -14,7 +14,7 @@
 #       AUTHOR: Charles Shih (schrht@gmail.com)
 # ORGANIZATION: ---
 #      CREATED: Fri Sep 24 11:37:17 AM CST 2021
-#     REVISION: Thu Oct 14 12:16:52 PM CST 2021
+#     REVISION: Mon Oct 18 09:03:11 AM CST 2021
 #==============================================================================
 
 trap cleanup SIGINT SIGTERM ERR EXIT
@@ -25,10 +25,10 @@ cleanup() {
 }
 
 # Setup environment
+mkdir -p /app/workspace
 if [ ! -e /root/.perf-insight.yaml ] && [ -f /data/config.yaml ]; then
     ln -s /data/config.yaml /root/.perf-insight.yaml
 fi
-mkdir -p /app/workspace
 
 # Start Flask as API server
 cd /opt/perf-insight/jupyter_server/
